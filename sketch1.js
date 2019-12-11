05874bf6b1a8f038873cfa8ed19f33bdba63b18f
Javier Ramirez <ramijavi@mail.gvsu.edu>
2019-12-11 20:35:28 +0000

    Update sketch1.js

--- sketch1.js
+++ sketch1.js
@@ -33,18 +33,3 @@ function draw()
     // Display the shape
     triangle(0, 0, -5, 15, 5, 15);
 }
-
-// draw an arrow for a vector at a given base position
-function drawArrow(base, vec, myColor) {
-    push();
-    stroke(myColor);
-    strokeWeight(3);
-    fill(myColor);
-    translate(base.x, base.y);
-    line(0, 0, vec.x, vec.y);
-    rotate(vec.heading());
-    let arrowSize = 7;
-    translate(vec.mag() - arrowSize, 0);
-    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-    pop();
-}
